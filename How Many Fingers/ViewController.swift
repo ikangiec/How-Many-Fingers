6ac966b268378570103ed7cbc187bf0ba2b3b027
Kangie Chan <aries0420@gmail.com>
2014-11-16 22:28:15 -0800

    Basic code to guess number of fingers.

--- How Many Fingers/ViewController.swift
+++ How Many Fingers/ViewController.swift
@@ -9,6 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var guess: UITextField!
+    @IBOutlet weak var message: UILabel!
+    
+    @IBAction func guessButton(sender: AnyObject) {
+        var numberOfFingers = arc4random() % 6
+        
+        println(numberOfFingers)
+        
+        var numberOfFingersString = String(numberOfFingers)
+        
+        if numberOfFingersString==guess.text {
+            message.text = "You got it right!"
+        } else {
+            if (numberOfFingers <= 1) {
+                message.text = "That wasn't right - I was holding up \(numberOfFingers) finger, Try again!"
+            }
+            else {
+                message.text = "That wasn't right - I was holding up \(numberOfFingers) fingers, Try again!"
+            }
+        }
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
